365cd34466314c93baefd02c4c0cf2cf42ebba64
nrismawardah <nrismawardah@gmail.com>
2023-03-16 15:59:39 +0700

    procedur bubble sort array

--- buble_sort/buble_sort.cpp
+++ buble_sort/buble_sort.cpp
@@ -25,8 +25,24 @@ void input() {          // procedur untuk input
         cin >> a[i];                            // Input dari pengguna
     }
 }
+void bubbleSortArray() {        // procedur untuk mengurutkan array dengan metode bubble sort
+    for (int i = 1; i < n; i++) {           // Looping dengan i dimulai dari 1 hingga n-1
+        for (int j = 0; j < n - i; j++) {   // Looping dengan j dimulai dari 0 hingga n-i-1
+            if (a[j] > a[j + 1]) {          // Jika nilai pada a[j] lebih besar dari a[j+1]
+                int temp = a[j];            // Simpan nilai a[j] ke variabel sementara temp
+                a[j] = a[j + 1];            // Assign nilai a[j+1] ke a[j]
+                a[j + 1] = temp;            // Assign nilai temp ke a[j+1]
+            }
+        }
+    }
+}
 
-
+void unsorted() {
+    cout << endl;               // Output baris kosong
+    cout << "===================" << endl;
+    cout << "Element Array yang belum tersusun" << endl;
+    for (int j = 0; j < n; j++) {}
+}
 
 int main()
 {
